6f83effd1a9a84abfdb7e61a424fc474798f1a1d
Mihai Doinea <mihai.doinea@oracle.com>
2022-11-13 10:09:04 +0200

    Android Service

--- app/src/main/java/ro/csie/en/androidservice/MainActivity.java
+++ app/src/main/java/ro/csie/en/androidservice/MainActivity.java
@@ -2,14 +2,59 @@ package ro.csie.en.androidservice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
     EditText etFirst, etSecond, etResult;
     Button button;
+
+    IService androidService;
+    ServiceConnection serviceConnection;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        serviceConnection = new ServiceConnection() {
+            @Override
+            public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+                androidService = IService.Stub.asInterface(iBinder);
+                Log.d(TAG,"Service was connected!");
+            }
+
+            @Override
+            public void onServiceDisconnected(ComponentName componentName) {
+                androidService = null;
+                Log.d(TAG,"Service was disconnected!");
+            }
+        };
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unbindService(serviceConnection);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(androidService == null)
+        {
+            Intent intent = new Intent(MainActivity.this, RemoteService.class);
+            bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +67,15 @@ public class MainActivity extends AppCompatActivity {
         button = findViewById(R.id.button);
 
         button.setOnClickListener(view -> {
-
+            String first = etFirst.getText().toString();
+            String second = etSecond.getText().toString();
+            String result = null;
+            try {
+                result = androidService.concatenate(first, second);
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+            etResult.setText(result);
         });
 
     }
